1f7bb5b368b2f4e5bfead5610e5df692b47e7ada
kano-e <till.cattial@gmail.com>
2015-05-29 00:48:28 +0900

    Add weather dummy

---
+++ scratchx-weather-dummy.js
@@ -0,0 +1,25 @@
+(function(ext) {
+  ext._shutdown = function() {};
+
+  ext._getStatus = function() {
+    return {status: 2, msg: 'Ready'};
+  };
+
+  ext.hello = function(callback) {
+    $.ajax({
+      url: 'https://kano-e.github.io/fftt218-scratchx/weather-dummy.json',
+      dataType: 'jsonp',
+      success: function(data) {
+        callback(data['weather']['title']);
+      }
+    });
+  };
+
+  var descriptor = {
+    blocks: [
+      ['r', 'weather dummy %s', 'weatherDummy']
+    ]
+  };
+
+  ScratchExtensions.register('weather dummy extension', descriptor, ext);
+})({});
