aa13ac45c99abf3b92ef38bd77cb7d0e148eb3cf
kano-e <till.cattail@gmail.com>
2015-05-28 21:41:06 +0900

    Fix indent

--- scratchx-hello.js
+++ scratchx-hello.js
@@ -1,19 +1,19 @@
 (function(ext) {
-    ext._shutdown = function() {};
+  ext._shutdown = function() {};
 
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+  ext._getStatus = function() {
+    return {status: 2, msg: 'Ready'};
+  };
 
-    ext.hello = function(str) {
-      return 'Hello ' + str;
-    };
+  ext.hello = function(str) {
+    return 'Hello ' + str;
+  };
 
-    var descriptor = {
-        blocks: [
-            ['r', 'hello %s', 'hello', 'Your name'],
-        ]
-    };
+  var descriptor = {
+    blocks: [
+      ['r', 'hello %s', 'hello', 'Your name']
+    ]
+  };
 
-    ScratchExtensions.register('Hello extension', descriptor, ext);
+  ScratchExtensions.register('Hello extension', descriptor, ext);
 })({});
